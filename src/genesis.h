96ebe9df4be1f17eee50a6704d8fc19a757528de
Sterlingcoin <spencer.lievens@sterlingcoin.org.uk>
2014-09-21 01:34:25 +0100

    Initial Commit

---
+++ src/genesis.h
@@ -0,0 +1,14 @@
+// By mammix2@hotmail.com
+// Moving the genesis block hashes out into a separate file makes it quicker to rebuild
+// multiple times when trying to find the genesis block hash 0
+
+#ifndef GENESIS_H
+#define GENESIS_H
+
+static const uint256 hashGenesisBlock("00000bc50b6bc35b06d2e2f38750b481703e43f51cd349173723785494bcc76d");
+static const uint256 hashGenesisBlockTestNet("00007f1f5389cc81d634159ec210563872251eb3cf4423d1fb438e0b1299b92f");
+
+
+
+
+#endif // GENESIS_H
